e93fcdc03225279248c07469624c2a287f08e73c
DJ <adminguest@DJs-MacBook-Pro.home>
2018-02-11 12:02:09 +0000

    added PITCHUP page and links on index.html

---
+++ comingsoon/js/custom.js
@@ -0,0 +1,38 @@
+(function($) {
+
+    /**************************************
+     * Loading BG Hide
+    ***************************************/
+    
+    $( document ).ready(function() {
+        setTimeout(function(){ 
+            $(".load-bg").addClass("animated bounceOutUp");
+        }, 3000);
+        setTimeout(function(){ 
+            $(".load-bar").addClass("animated fadeOut");
+        }, 3300);
+        
+    });
+
+    /**************************************
+     * Header Background Change
+    ***************************************/
+    $(function(){
+     var shrinkHeader = 625;
+      $(window).scroll(function() {
+        var scroll = getCurrentScroll();
+            if ( scroll >= shrinkHeader ) {
+                $('.home').addClass('body-scroll');
+                $('#headerlogo').addClass('animated fadeOut');
+            }
+            else {
+                $('.home').removeClass('body-scroll');
+            }
+      });
+    function getCurrentScroll() {
+        return window.pageYOffset || document.documentElement.scrollTop;
+        }
+    });
+
+
+})(jQuery);
